92d3a7dbe339a20d1b5a96c9be0a4480e1de06f2
RAED <rrrr53@hotmail.com>
2017-02-13 07:02:50 +0300

    Update enigma.cpp

--- main/enigma.cpp
+++ main/enigma.cpp
@@ -9,7 +9,6 @@
 #include <lib/actions/action.h>
 #include <lib/driver/rc.h>
 #include <lib/base/ioprio.h>
-#include <lib/base/e2avahi.h>
 #include <lib/base/ebase.h>
 #include <lib/base/eenv.h>
 #include <lib/base/eerror.h>
@@ -113,7 +112,6 @@ class eMain: public eApplication, public Object
 public:
 	eMain()
 	{
-	        e2avahi_init(this);
  		init_servicepeer();
 		init.setRunlevel(eAutoInitNumbers::main);
 		/* TODO: put into init */
@@ -129,7 +127,6 @@ public:
 		m_dvbdb->saveServicelist();
 		m_mgr->releaseCachedChannel();
 		done_servicepeer();
-  		e2avahi_close();
 	}
 };
 
